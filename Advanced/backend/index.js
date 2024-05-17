1d602cf0557c77fae0d216941a7dbe759032a2e2
Subhajit Bhattacharya <subhajitbhattacharya08@gmail.com>
2024-05-17 14:09:37 +0530

    Removing .env file

--- Advanced/backend/index.js
+++ Advanced/backend/index.js
@@ -1,10 +1,19 @@
-import {} from 'dotenv/config'
+import dotenv from 'dotenv/config'
 // import mongoose from 'mongoose';
 // import { DB_NAME } from './constants';
 import connectDB from './src/db/connectDatabase.js'
+import { app } from './src/app.js'
 
 // dotenv.config({
 //     path: './config.env'
 // })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed !!!", err);
+})
